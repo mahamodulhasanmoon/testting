8265cec547bdf00964a4e1d3ea65c13d521533cb
Mahamodul Hasan Moon <mahamodulhasan.moon@gmail.com>
2024-06-28 14:08:44 +0600

    test

---
+++ bin/cli.js
@@ -0,0 +1,29 @@
+#!/usr/bin/env node
+/* eslint-disable no-console */
+
+import  { execSync } from 'child_process';
+
+const runCommand = (command) => {
+  try {
+    execSync(`${command}`, { stdio: 'inherit' });
+  } catch (error) {
+    console.log(`Failed to execute ${command}`);
+    return false;
+  }
+  return true;
+};
+
+const repoName = process.argv[2];
+const gitCheckoutCommand = `git clone --depth 1 https://github.com/mahamodulhasanmoon/rapid-expresskit ${repoName}`;
+const installDepsCommand = `cd ${repoName} && npm install`;
+
+console.log('Downloading boilerplate...');
+const checkout = runCommand(gitCheckoutCommand);
+if (!checkout) process.exit(-1);
+
+console.log('Installing dependencies...');
+const installDeps = runCommand(installDepsCommand);
+if (!installDeps) process.exit(-1);
+
+console.log('Congratulations! You are ready to start.');
+console.log(`cd ${repoName} && npm start`);
